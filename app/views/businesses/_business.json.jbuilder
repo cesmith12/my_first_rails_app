a3b59446c30b6ef9e487e51eff0f7012aac6375b
Chris Smith <nigradeus@gmail.com>
2019-01-10 11:20:35 -0500

    initial commit

---
+++ app/views/businesses/_business.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! business, :id, :job, :responsibility, :year, :created_at, :updated_at
+json.url business_url(business, format: :json)
---
+++ db/migrate/20190110161538_create_businesses.rb
@@ -0,0 +1,11 @@
+class CreateBusinesses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :businesses do |t|
+      t.string :job
+      t.string :responsibility
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
